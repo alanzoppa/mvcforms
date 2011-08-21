a5ab049b5a636b1a2af882a3637fb9245958f8c7
Alan Zoppa <alan.zoppa@gmail.com>
2011-08-21 13:33:36 -0500

    Django-style ids and names

--- forms.rb
+++ forms.rb
@@ -1,15 +1,19 @@
 
 class Field
-  attr_accessor :type, :label, :help_text, :html_id
+  attr_accessor :type, :label, :name, :help_text, :html_id
 
   def initialize(type, attributes=nil, label=nil, help_text=nil)
     @type, @attributes = type, attributes
-    raise "Field.html_id is defined by the class value the field is set to" if attributes.has_key?(:id)
+    unless attributes.nil?
+      raise "Field.html_id is defined by the class value the field is set to" if attributes.has_key?(:id)
+      raise "Field.name is defined by the class value the field is set to" if attributes.has_key?(:name)
+    end
   end
 
   def to_html
     value_pairs = @attributes.to_a.map {|key,value| "#{key}='#{value}'"}
-    value_pairs << "id='#{self.html_id}'"
+    value_pairs << "name='#{self.name}'"
+    value_pairs << "id='id_#{self.name}'"
     value_pairs = value_pairs.join ' '
     return "<input type='#{self.type}' #{value_pairs} />"
   end
@@ -29,8 +33,8 @@ class Form
         [ self.class.class_variable_get(v), v.to_s.gsub(/^@@/, '') ]
       end
     }
-    field_pairs.each do |field, field_id|
-      field.html_id = field_id.to_sym
+    field_pairs.each do |field, field_name|
+      field.name = field_name.to_sym
       @fields << field
     end
   end
--- forms_spec.rb
+++ forms_spec.rb
@@ -4,17 +4,17 @@ describe "Low level Field behavior" do
 
   before do
     class DerpForm < Form
-      @@derp_field = Field.new(:text, {:name => :bar})
+      @@derp_field = Field.new(:text)
     end
     @test_field = DerpForm.new.fields[0]
   end
 
   it "should assign its id based on the class variable name" do
-    @test_field.html_id.should == :derp_field
+    @test_field.name.should == :derp_field
   end
 
   it "should be able to template basic field types" do
-    @test_field.to_html.should == "<input type='text' name='bar' id='derp_field' />"
+    @test_field.to_html.should == "<input type='text' name='derp_field' id='id_derp_field' />"
   end
 
 end
@@ -24,13 +24,13 @@ describe "Low level Form behavior" do
 
   before do
     class LoginForm < Form
-      @@username = Field.new(:text, :name => "username")
-      @@password = Field.new(:text, :name => :password, :class => :pw)
+      @@username = Field.new(:text,)
+      @@password = Field.new(:text, :class => :pw)
     end
     @login_form = LoginForm.new
 
     class OptInForm < Form
-      @@future_communications = Field.new(:checkbox, :checked => :checked, :name => :future_communications, )
+      @@future_communications = Field.new(:checkbox, :checked => :checked )
     end
     @opt_in_form = OptInForm.new
   end
@@ -38,15 +38,15 @@ describe "Low level Form behavior" do
   it "should correctly report its fields in the defined order" do
     fields_as_strings = @login_form.fields.map {|f| f.to_html}
     fields_as_strings.should == [
-      "<input type='text' name='username' id='username' />",
-      "<input type='text' name='password' class='pw' id='password' />",
+      "<input type='text' name='username' id='id_username' />",
+      "<input type='text' class='pw' name='password' id='id_password' />",
     ]
   end
 
   it "should accept a hash of attributes" do
     fields_as_strings = @opt_in_form.fields.map {|f| f.to_html}
     fields_as_strings.should == [
-      "<input type='checkbox' checked='checked' name='future_communications' id='future_communications' />"
+      "<input type='checkbox' checked='checked' name='future_communications' id='id_future_communications' />"
     ]
   end
 
