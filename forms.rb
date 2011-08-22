5d35a2510d355dc51af0fee3cf1c3d626a6948f9
Alan Zoppa <alan.zoppa@gmail.com>
2011-08-22 10:09:52 -0500

    approaching a working RadioChoiceField

--- forms.rb
+++ forms.rb
@@ -2,18 +2,22 @@
 class Field
   attr_accessor :type, :label_text, :name, :help_text, :html_id
 
-  def initialize(type, label_text=nil, attributes=nil, help_text=nil )
-    @type, @label_text, @help_text, @attributes = type, label_text, help_text, attributes
-    unless attributes.nil?
-      raise "Field.html_id is defined by the class value the field is set to" if attributes.has_key?(:id)
-      raise "Field.name is defined by the class value the field is set to" if attributes.has_key?(:name)
+  def _standard_validation
+    unless @attributes.nil?
+      raise "Field.html_id is defined by the class value the field is set to" if @attributes.has_key?(:id)
+      raise "Field.name is defined by the class value the field is set to" if @attributes.has_key?(:name)
     end
+  end
+
+  def initialize(label_text=nil, attributes=nil, help_text=nil )
+    @label_text, @help_text, @attributes = label_text, help_text, attributes
+    _standard_validation
+    @type = self.class.to_s.gsub(/Field$/, '').downcase
   end
 
   def to_html
     value_pairs = @attributes.to_a.map {|key,value| "#{key}='#{value}'"}
-    value_pairs << "name='#{self.name}'"
-    value_pairs << "id='#{self.html_id}'"
+    value_pairs << ["name='#{self.name}'", "id='#{self.html_id}'"]
     value_pairs = value_pairs.join ' '
     return "<input type='#{self.type}' #{value_pairs} />"
   end
@@ -24,24 +28,50 @@ class Field
 
 end
 
+class RadioField < Field
+  def initialize(label_text, value, name)
+    @label_text, @value, @name = label_text, value, name
+    @type = :radio
+    @html_id = "id_#{@name}_#{@value}"
+  end
+
+  def to_labeled_html
+    "<input type='#{@type}' name='#{@name}' id='#{@html_id}' value='#{@value}'><label for='#{@html_id}'>#{@label_text}</label>"
+  end
+end
+
+class RadioChoiceField < Field
+  def initialize(value_array, attributes, help_text)
+  end
+end
+
+class TextField < Field
+end
+
+class CheckboxField < Field
+end
 
 class Form
   attr_accessor :fields
+
+  def _attach_field_name(field, field_name)
+      field.name = field_name.to_sym
+      field.html_id = "id_#{field_name}".to_sym
+      @fields << field
+  end
 
   def initialize 
     @fields = Array.new
 
     # All class variables which are instances of Field
-    field_pairs = self.class.class_variables.map { |v|
-      if self.class.class_variable_get(v).class == Field
-        [ self.class.class_variable_get(v), v.to_s.gsub(/^@@/, '') ]
+    field_pairs = Array.new
+    self.class.class_variables.each do |v|
+      #if [TextField, CheckboxField, RadioChoiceField].include?(field.class)
+      if field.class.superclass == Field
+        _attach_field_name( field, v.to_s.gsub(/^@@/, '') )
       end
-    }
-    field_pairs.each do |field, field_name|
-      field.name = field_name.to_sym
-      field.html_id = "id_#{field_name}".to_sym
-      @fields << field
     end
+
   end
 
 end
--- forms_spec.rb
+++ forms_spec.rb
@@ -4,9 +4,11 @@ describe "Low level Field behavior" do
 
   before do
     class DerpForm < Form
-      @@derp_field = Field.new(:text, "Herp some derps")
+      @@derp_field = TextField.new("Herp some derps")
+      @@radio = RadioField.new("Male", "male", "sex")
     end
     @test_field = DerpForm.new.fields[0]
+    @radio_field = DerpForm.new.fields[1]
   end
 
   it "should assign its id based on the class variable name" do
@@ -19,7 +21,36 @@ describe "Low level Field behavior" do
 
   it "should generate its own labels" do
     @test_field.label.should == "<label for='id_derp_field'>Herp some derps</label>"
+    @test_field.label_text.should == "Herp some derps"
   end
+
+  it "should create radio sub fields" do
+    @radio_field.to_labeled_html.should == "<input type='radio' name='sex' id='id_sex_male' value='male'><label for='id_sex_male'>Male</label>"
+  end
+
+  #it "should support radio buttons" do
+    #class GenderForm < Form
+      #gender_options = [ { :value => "male", :label => "Male", },
+                         #{ :value => "female", :label => "Female", }, ]
+
+      #@@sex = RadioChoiceField.new(gender_options, :checked => :checked,)
+    #end
+    #@gender_form = GenderForm.new
+
+    #@gender_form.fields.should == [
+      #"<input type='radio' name='sex' value='male' />",
+      #"<input type='radio' name='sex' value='female' />"
+    #]
+
+
+#<fieldset>
+#<input type="radio" name="group1" id="rad1" value="1"><label for="rad1">button one</label>
+#<input type="radio" name="group1" id="rad2" value="2"><label for="rad2">button two</label>
+#<input type="radio" name="group1" id="rad3" value="3">klabel for="rad3">button three</label>
+#</fieldset>
+
+
+  #end
 
 end
 
@@ -28,15 +59,12 @@ describe "Low level Form behavior" do
 
   before do
     class LoginForm < Form
-      @@username = Field.new(:text, "Username")
-      @@password = Field.new(:text, "Password", :class => :pw,)
+      @@username = TextField.new("Username")
+      @@password = TextField.new("Password", :class => :pw,)
+      @@herp = "derp"
     end
     @login_form = LoginForm.new
 
-    class OptInForm < Form
-      @@future_communications = Field.new(:checkbox, "Would you like to receive future communications", {:checked => :checked}, )
-    end
-    @opt_in_form = OptInForm.new
   end
 
   it "should correctly report its fields in the defined order" do
@@ -48,6 +76,11 @@ describe "Low level Form behavior" do
   end
 
   it "should accept a hash of attributes" do
+    class OptInForm < Form
+      @@future_communications = CheckboxField.new("Would you like to receive future communications", :checked => :checked, )
+    end
+    @opt_in_form = OptInForm.new
+
     fields_as_strings = @opt_in_form.fields.map {|f| f.to_html}
     fields_as_strings.should == [
       "<input type='checkbox' checked='checked' name='future_communications' id='id_future_communications' />"
