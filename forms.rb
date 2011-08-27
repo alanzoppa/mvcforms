bef4d025d81b68248d3597261830a8aeac49b431
Alan Zoppa <alan.zoppa@gmail.com>
2011-08-26 23:41:48 -0500

    Higher-level Form methods

--- forms.rb
+++ forms.rb
@@ -5,28 +5,55 @@ class Form
 
   def initialize 
     _initialize_fields
+    _prepare_getters
   end
 
   def _initialize_fields
     @fields = Array.new
     self.class.class_variables.each { |v|
-      _flatten_fields field=self.class.class_variable_get(v), field_name=v.to_s.gsub(/^@@/, '')
+      __flatten_fields field=self.class.class_variable_get(v), field_name=v.to_s.gsub(/^@@/, '')
     }
   end
 
-  def _flatten_fields(field, field_name)
+  def __flatten_fields(field, field_name)
     if field.class == Array && field.all? {|f| f.class.superclass == Field}
       raise "Fields must be of the same type" unless field.all? {|f| f.class == field[0].class }
-      field.each {|f| _attach_field_name(f, field_name) }
+      field.each {|f| ___attach_field_name(f, field_name) }
     elsif field.class.superclass == Field
-      _attach_field_name(field, field_name)
+      ___attach_field_name(field, field_name)
     end
   end
 
-  def _attach_field_name(field, field_name)
+  def ___attach_field_name(field, field_name)
     field.name = field_name.to_sym
     field.attach_names!(field_name) if field.respond_to?(:attach_names!)
     @fields << field
+  end
+
+  def _prepare_getters
+    @queryable_structures = Hash.new
+    @fields.each do |field|
+      @queryable_structures[field.name.to_sym] = {
+        :field => field.to_html,
+        :label_tag => field.label_tag,
+      }
+    end
+  end
+
+  def get_group field
+    return @queryable_structures[field.to_sym]
+  end
+
+  def get(type, field)
+    return get_group(field.to_sym)[type.to_sym]
+  end
+
+  def get_field field
+    return get_group(field)[:field]
+  end
+
+  def get_label_tag field
+    return get_group(field)[:label_tag]
   end
   
 end
--- forms_spec.rb
+++ forms_spec.rb
@@ -14,9 +14,21 @@ describe "A Form with a TextField" do
     @derp_field = DerpForm.new.fields[0]
   end
 
-  #it "should be able to select individual fields" do
-    #@derp_field.get_group(:derp_field)[:field].should == "<input type='text' name='derp_field' id='id_derp_field' />"
-  #end
+  it "should be able to select a Hash of field attributes" do
+    @derp_form.get_group(:derp_field)[:field].should == "<input type='text' name='derp_field' id='id_derp_field' />"
+  end
+
+  it "should be able to select individual label tags" do
+    puts @derp_form.get_group(:derp_field)[:label_tag].should == "<label for='id_derp_field'>Herp some derps</label>"
+  end
+
+  it "should be able to get individual fields" do
+    @derp_form.get(:field, :derp_field).should == "<input type='text' name='derp_field' id='id_derp_field' />"
+  end
+
+  it "should be able to get individual labels" do
+    puts @derp_form.get(:label_tag, :derp_field).should == "<label for='id_derp_field'>Herp some derps</label>"
+  end
 
 end
 
